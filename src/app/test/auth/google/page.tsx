329534b8777336755df95c0bdbe695184e0e7cfe
Sajid Hassan <sajidhassancs@gmail.com>
2024-06-16 14:18:27 +0500

    djjjjjjj

--- src/app/test/auth/google/page.tsx
+++ src/app/test/auth/google/page.tsx
@@ -1,12 +1,21 @@
-import Google from "./Google";
+// import Google from "./Google";
+
+// const page = () => {
+//   return (
+//     <div>
+//       Google Page
+//       <Google />
+//     </div>
+//   );
+// };
+
+// export default page;
+import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Google Page
-      <Google />
-    </div>
-  );
-};
+    <div>page</div>
+  )
+}
 
-export default page;
+export default page
