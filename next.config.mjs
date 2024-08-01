94960464d3653c1a8822627dfc1b8325a5aab2cb
Sajid Hassan <sajidhassancs@gmail.com>
2024-08-01 17:06:57 +0500

    some tkgdkcg unchange

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,30 @@
 // next.config.mjs
-import withTM from 'next-transpile-modules';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'engine.io-client': require.resolve('engine.io-client'),
-      'engine.io-parser': require.resolve('engine.io-parser'),
-    };
-    return config;
+  images: {
+    domains: ['localhost', 'ajs-files.hostdonor.com','example.com'], 
   },
 };
+          
+export default nextConfig;
 
-export default withTM(['engine.io-client', 'engine.io-parser', 'socket.io-client'])(nextConfig);
+
+
+// // next.config.mjs
+// import withTM from 'next-transpile-modules';
+// import { createRequire } from 'module';
+
+// const require = createRequire(import.meta.url);
+
+// const nextConfig = {
+//   webpack: (config) => {
+//     config.resolve.alias = {
+//       ...config.resolve.alias,
+//       'engine.io-client': require.resolve('engine.io-client'),
+//       'engine.io-parser': require.resolve('engine.io-parser'),
+//     };
+//     return config;
+//   },
+// };
+
+// export default withTM(['engine.io-client', 'engine.io-parser', 'socket.io-client'])(nextConfig);
