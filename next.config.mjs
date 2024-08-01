34c4fe3fbde7fa7e6deabf4545e556d9672e6367
Sajid Hassan <sajidhassancs@gmail.com>
2024-08-01 14:50:18 +0500

    53333

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
+// next.config.mjs
 import withTM from 'next-transpile-modules';
 
-export default withTM({
-  transpileModules: ['engine.io-parser', 'socket.io-client'],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ['localhost', 'ajs-files.hostdonor.com', 'example.com'],
   },
-});
+};
+
+export default withTM(['engine.io-parser', 'socket.io-client'])(nextConfig);
