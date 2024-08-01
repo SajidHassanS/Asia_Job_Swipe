e56173f13d92284eba6cb34c2e9111a4c8e4eb8d
Sajid Hassan <sajidhassancs@gmail.com>
2024-08-01 15:19:40 +0500

    some chaneeggr

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 // next.config.mjs
-import withTM from 'next-transpile-modules';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'ajs-files.hostdonor.com', 'example.com'],
+    domains: ['localhost', 'ajs-files.hostdonor.com','example.com'], 
   },
 };
-
-export default withTM(['engine.io-parser', 'socket.io-client'])(nextConfig);
+          
+export default nextConfig;
--- src/components/SocketListener.tsx
+++ src/components/SocketListener.tsx
@@ -1,22 +1,22 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import socket from '@/services/socket';
-import { receiveMessage } from '@/store/slices/messageSlice'; // Adjust the path as necessary
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import socket from '@/services/socket';
+// import { receiveMessage } from '@/store/slices/messageSlice'; // Adjust the path as necessary
 
-const SocketListener: React.FC = () => {
-  const dispatch = useDispatch();
+// const SocketListener: React.FC = () => {
+//   const dispatch = useDispatch();
 
-  useEffect(() => {
-    socket.on('newMessage', (message) => {
-      dispatch(receiveMessage(message));
-    });
+//   useEffect(() => {
+//     socket.on('newMessage', (message) => {
+//       dispatch(receiveMessage(message));
+//     });
 
-    return () => {
-      socket.off('newMessage');
-    };
-  }, [dispatch]);
+//     return () => {
+//       socket.off('newMessage');
+//     };
+//   }, [dispatch]);
 
-  return null;
-};
+//   return null;
+// };
 
-export default SocketListener;
+// export default SocketListener;
