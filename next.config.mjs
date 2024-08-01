3cfe27e2d95b4c737a99031feb95f0a33dc14628
Sajid Hassan <sajidhassancs@gmail.com>
2024-08-01 14:18:29 +0500

    sslibmee 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 // next.config.mjs
+import engineIoParser from 'engine.io-parser';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['localhost', 'ajs-files.hostdonor.com', 'example.com'], 
   },
   webpack: (config) => {
-    // Add fallback for engine.io-parser if needed
     config.resolve.fallback = {
       ...config.resolve.fallback,
-      'engine.io-parser': require.resolve('engine.io-parser'),
+      'engine.io-parser': engineIoParser,
     };
     return config;
   },
