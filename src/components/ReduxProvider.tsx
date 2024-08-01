0fc771c690ff0e5300dd2c6e06d373ff0e30b705
Sajid Hassan <sajidhassancs@gmail.com>
2024-08-01 22:01:44 +0500

    jkdkffdkeeef

--- src/components/ReduxProvider.tsx
+++ src/components/ReduxProvider.tsx
@@ -5,8 +5,8 @@ import { Provider } from 'react-redux';
 import store from '../store';
 import AppInitializer from '../store/slices/AppInitializer'; // Ensure this import path is correct
 // import SocketListener from '@/components/SocketListener'; 
-import MessageListener from '@/services/MessageListener';
-import Socket from '@/services/socket';
+// import MessageListener from '@/services/MessageListener';
+// import Socket from '@/services/socket';
 const ReduxProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider store={store}>
